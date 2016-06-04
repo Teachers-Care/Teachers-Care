d2cc5a36cf547483b7a8975229752aa584b55e3d
Mallerie Shirley <mallerie@gmail.com>
2016-06-04 19:28:10 -0400

    mapped student names

--- src/js/admin.js
+++ src/js/admin.js
@@ -7,18 +7,41 @@ import data from './student-data.js';
 
 export default class Admin extends Component {
 
-  // static propTypes = {
-  //   students: PropTypes.array.isRequired
+  // getRisks(risk) {
+
+  //   let { risk } = data.reports.risks;
+  //   return (
+  //     <div className="risk-item">
+  //       {risk.riskID}
+  //     </div>
+  //     )
+  // }
+
+  // getReports(report) {
+  //   let { report } = data.reports;
+  //   return (
+  //     <div className="report-item">
+  //       <div>{report.registered}</div>
+  //       <div>{report.comment}</div>
+  //       <div className="risks">
+  //         {data.map(::this.getRisks)}
+  //       </div>
+
+  //     </div>
+
+  //     )
   // }
 
   getStudent(student) {
-    let { data } = this.props;
 
     return (
-      <div className="student-item">
-        <div>{data.name.first}</div>
-        <div>{data.name.last}</div>
-        <div>{data.grade}</div>
+      <div className="student-item" key={student._id}>
+        <div>{student.name.first}</div>
+        <div>{student.name.last}</div>
+        <div>{student.grade}</div>
+        <div className="reports">
+          {/*{data.map(this.getReports)}*/}
+        </div>
       </div>
 
       )
@@ -26,19 +49,14 @@ export default class Admin extends Component {
 
     render() {
 
-      let { students } = this.props;
-
       return(
 
         <div className="admin">
           <div>Admin PAge</div>
-          <div className="students-wrapper">{students.map(::this.getStudent)}</div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
-          <div><img src="http://fillmurray.com/30/30"/></div>
+          <div className="students-wrapper">
+            {data.map(this.getStudent)}
+            </div>
+          
 
         </div>
 
