3fb05ca46a2ca859513e399a59b1071a6ef0ef73
Alex Windle <alexwindle85@gmail.com>
2016-06-04 14:37:59 -0400

    test new form

--- src/js/report-a-student.js
+++ src/js/report-a-student.js
@@ -13,14 +13,48 @@ export default class ReportAStudent extends Component {
 
 			<div>
 				<SSF>
-					<div>Report A Student Form</div>
-
-					<div>Student Name</div>
-
-					<div>Grade</div>
-
-					<div>Form </div>
-					
+					<h1>Student Note</h1>
+					<p>Select a concern and submit anonymously.</p>
+					<label>
+						(1)
+						<input type="checkbox" />
+						Is dating a significantly older person.
+					</label>
+					<label>
+						(2)
+						<input type="checkbox" />
+						Frequently travels with an older person who is not their guardian.
+					</label>
+					<label>
+						(3)
+						<input type="checkbox" />
+						Freqently absent from class. 
+					</label>
+					<label>
+						(4)
+						<input type="checkbox" />
+						Suddenly changed appearance or style, especially if of a provocative nature.
+					</label>
+					<label>
+						(5)
+						<input type="checkbox" />
+						Frequently travels with an older person who is not their guardian.
+					</label>
+					<label>
+						(6)
+						<input type="checkbox" />
+						Frequently travels with an older person who is not their guardian.
+					</label>
+					<label>
+						(7)
+						<input type="checkbox" />
+						Frequently travels with an older person who is not their guardian.
+					</label>
+					<label>
+						(8)
+						<input type="checkbox" />
+						Frequently travels with an older person who is not their guardian.
+					</label>
 					<button>Submit</button>
 				</SSF>
 			</div>
