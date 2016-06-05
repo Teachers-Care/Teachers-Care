df5c250cff034b0e9b5272f30a4b2dc35fbcbf95
Alex Windle <alexwindle85@gmail.com>
2016-06-05 10:52:39 -0400

    remove resources link in 'thank you' view

--- src/js/thank-you.js
+++ src/js/thank-you.js
@@ -9,20 +9,12 @@ export default class ThankYou extends Component {
   render() {
     return(
       <div className="thank-you">
-
-
           <h2>You're making a difference!</h2>
-
           <p>Thank you for participating in the Teachers CARE initiative. Your participation brings us one step closer to eliminating child pretators.</p>
-
           <iframe 
             src="https://www.youtube.com/embed/vGgDj-xZJ7o">
           </iframe>
-
-          <Link to="/resources">Additional Resources</Link>
-
-        </div>
-
-      )
+      </div>
+    )
   }
 }
