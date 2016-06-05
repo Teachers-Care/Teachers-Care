966db4203d5efe539677ec56380199ffce027bab
Mallerie Shirley <mallerie@gmail.com>
2016-06-04 20:55:54 -0400

    Work in Progress

--- src/js/heading.js
+++ src/js/heading.js
@@ -12,12 +12,9 @@ export default class Heading extends Component {
 				<div><h2>100% anonymous tip-line.</h2></div>
 					{this.props.children}
 				<footer>
-<<<<<<< HEAD
-					<div><Link to="/admin"><button className="small-button">For Administrators</button></Link></div>
-					<div>&copy; 2016</div>
-=======
+
 						<div>&copy; 2016</div>
->>>>>>> bead08fd4c789d739032c67fd1e00fe70dbfe1d2
+
 				</footer>
 			</div>
 		)
