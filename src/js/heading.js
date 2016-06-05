656b516b8ead02eefff0df8c78e34f7321e8f455
Alex Windle <alexwindle85@gmail.com>
2016-06-04 20:58:32 -0400

    header link to home

--- src/js/heading.js
+++ src/js/heading.js
@@ -8,16 +8,15 @@ export default class Heading extends Component {
 	render(){
 		return(
 			<div className="heading">
-				<div><h1>A Helping Hand</h1></div>
+				<div>
+					<Link to="/">
+						<h1>A Helping Hand</h1>
+					</Link>
+				</div>
 				<div><h2>100% anonymous tip-line.</h2></div>
 					{this.props.children}
 				<footer>
-<<<<<<< HEAD
-					<div><Link to="/admin"><button className="small-button">For Administrators</button></Link></div>
 					<div>&copy; 2016</div>
-=======
-						<div>&copy; 2016</div>
->>>>>>> bead08fd4c789d739032c67fd1e00fe70dbfe1d2
 				</footer>
 			</div>
 		)
