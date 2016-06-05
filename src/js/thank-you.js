1a733632c5ca530595fc3b56f9c3771f3f642795
Alex Windle <alexwindle85@gmail.com>
2016-06-05 10:54:25 -0400

    spelling fix

--- src/js/thank-you.js
+++ src/js/thank-you.js
@@ -10,7 +10,7 @@ export default class ThankYou extends Component {
     return(
       <div className="thank-you">
           <h2>You're making a difference!</h2>
-          <p>Thank you for participating in the Teachers CARE initiative. Your participation brings us one step closer to eliminating child pretators.</p>
+          <p>Thank you for participating in the Teachers CARE initiative. Your participation brings us one step closer to eliminating child predators.</p>
           <iframe 
             src="https://www.youtube.com/embed/vGgDj-xZJ7o">
           </iframe>
