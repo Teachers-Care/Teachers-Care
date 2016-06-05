7170b13eed29e1692e91d5e61d313f53e84c2e8b
Jennifer Ford <jenncford@gmail.com>
2016-06-05 10:53:09 -0400

    Finalliy got table to show up

---
+++ src/js/data.js
@@ -0,0 +1,12 @@
+var student = [{
+    "first" : "Jason",
+    "last" : "Mason",
+    "age" : "11",
+    "gender" : "male"
+},
+{
+    "first" : "Kylie",
+    "last" : "Peterson",
+    "age" : "12",
+    "gender" : "female"
+}];
